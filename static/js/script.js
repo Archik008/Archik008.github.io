b32f58a51424280c220f476c190044570ec37862
Артем <119063410+Archik008@users.noreply.github.com>
2024-10-09 21:36:07 +0600

    Добавляю еще раз файлы 6 Лабы

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let url = "https://jsonplaceholder.typicode.com/users"
 
 fetch(url)
@@ -44,49 +43,3 @@ fetch(url)
     .catch((error) => {
         throw new Error(error);
     })
-=======
-
-let button = document.querySelector('.button button')
-
-button.onclick = function () {
-    let num1 = document.querySelector('.input1 input')
-    let num2 = document.querySelector('.input2 input')
-    
-    num1 = Number(num1.value)
-    num2 = Number(num2.value)
-
-    if (num1 > num2) {
-        alert(`Num1(${num1}) > Num2(${num2})`)
-    }
-    else if (num1 == num2) {
-        alert(`Num1(${num1}) = Num2(${num2})`)
-    }
-    else{
-        alert(`Num1(${num1}) < Num2(${num2})`)
-    }
-}
-
-let var1 = null
-let var2 = 23
-let var3 = true
-
-let buttonCompile = document.querySelector('.button-compile button')
-
-buttonCompile.onclick = function () {
-    document.querySelector('.result').innerHTML = `<span class="keyword">${typeof var1}</span><br>
-    <span class="keyword">${typeof var2}</span> <br>
-    <span class="keyword">${typeof var3}</span> <br>`
-}
-
-let buttonCompare = document.querySelector('.button-compare button')
-
-buttonCompare.onclick = function () {
-    let numCompare = document.querySelector('.input3 input').value
-    if (numCompare % 2 == 0) {
-        alert('Число четное')
-    }
-    else{
-        alert('Число нечетное')
-    }
-}
->>>>>>> 474f0b6e761f7a7fd18485c7902636c1ab921098
