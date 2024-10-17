1c7cdc0646e67ab712cc0d2dd74cb66a225e6849
Артем <119063410+Archik008@users.noreply.github.com>
2024-10-17 16:11:40 +0600

    Добавление чужих анимаций

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,7 @@ function handleMediaQuery(event) {
     if (event.matches) {
         setMobile();
     } else {
-        document.querySelector('.slack-android-container').style.display = 'none';
+        setDesktop();
     }
 }
 
@@ -15,10 +15,25 @@ function setMobile() {
     slack_android.addEventListener('touchstart', (e) => {
         e.preventDefault();
         slack_android.style.backgroundColor = 'rgb(224, 224, 77)';
+        slack_android.classList.add('glow-effect');
     });
 
     slack_android.addEventListener('touchend', () => {
         slack_android.style.backgroundColor = 'rgb(248, 248, 80)';
+        slack_android.classList.remove('glow-effect');
+    })
+}
+
+function setDesktop() {
+    document.querySelector('.slack-android-container').style.display = 'none';
+
+    const get_started_btn = document.querySelector('.some-words .get-started')
+    get_started_btn.addEventListener('mouseenter', () => {
+        get_started_btn.classList.add('glitch-effect');
+    })
+
+    get_started_btn.addEventListener('mouseout', () => {
+        get_started_btn.classList.remove('glitch-effect');
     })
 }
 
