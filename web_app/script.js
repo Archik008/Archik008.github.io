69644768279d9736cc41b514ae7ea62142048180
Артем <119063410+Archik008@users.noreply.github.com>
2024-11-14 20:26:25 +0600

    Финальная правка

--- web_app/script.js
+++ web_app/script.js
@@ -1,24 +1,56 @@
-const tg = window.Telegram
+const tgWebApp = window.Telegram
 
-let service = null
+let service = null;
 
 function get_service(elm) {
     const spanServiceText = elm.querySelector('span').textContent
 
     service = spanServiceText;
 
-    console.log(service)
+    console.log(service);
 
     const questionBlock = document.querySelector(".question_main_block");
-    questionBlock.style.display = "none";
+    questionBlock.classList.remove("visible");
+    questionBlock.classList.add("hidden");
 
-    document.querySelector(".form-main-block").style.display = "block";
+    // Появление блока формы после задержки
+    const formBlock = document.querySelector(".form-main-block");
+
+    setTimeout(() => {
+        questionBlock.style.display = "none"; // скрываем элемент после анимации
+
+        formBlock.style.display = "block"; // показываем блок перед анимацией
+        formBlock.classList.remove("hidden");
+        formBlock.classList.add("visible");
+    }, 500); // задержка должна совпадать с продолжительностью анимации
 }
 
-const theme = Telegram.WebApp.themeParams;
+const theme = tgWebApp.themeParams;
 
 document.documentElement.style.setProperty('--tg-theme-bg-color', theme.bg_color || '#ffffff');
 document.documentElement.style.setProperty('--tg-theme-text-color', theme.text_color || '#000000');
 document.documentElement.style.setProperty('--tg-theme-hint-color', theme.hint_color || '#cccccc');
 document.documentElement.style.setProperty('--tg-theme-button-color', theme.button_color || '#4a76a8');
 document.documentElement.style.setProperty('--tg-theme-button-text-color', theme.button_text_color || '#ffffff');
+
+
+const submitBtn = document.querySelector(".send").querySelector("input")
+
+submitBtn.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const contact = document.getElementById('contact').value;
+    const description = document.getElementById('description').value;
+
+    const formData = {
+        name: name,
+        contact: contact,
+        description: description,
+        service_type: service
+    };
+
+    tgWebApp.sendData(JSON.stringify(formData));
+
+    tgWebApp.close()
+})
